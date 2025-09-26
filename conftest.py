7e80799c04ba2690b44810d5e799c52d152bce7c
Your Name <you@example.com>
2025-09-27 00:35:34 +0300

    test: add global conftest to expose src/ and workdir/ on sys.path; flatten tests tree (#22)

---
+++ conftest.py
@@ -0,0 +1,5 @@
+﻿import sys, pathlib
+
+ROOT = pathlib.Path(__file__).parent.resolve()
+# src ve workdir'i en başa ekle (çakışma riskini azaltır)
+sys.path[:0] = [str(ROOT / "src"), str(ROOT / "workdir")]
